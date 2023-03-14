e7fba17d4a62701080df95dd3e08d11983855aa3
guilhermedkdk <guilhermeperesmm@gmail.com>
2023-03-14 17:34:32 -0300

    finishing routes

--- routes/order.js
+++ routes/order.js
@@ -8,7 +8,7 @@ router.post("/", verifyToken, async (req, res) => {
     const newOrder = new Order(req.body)
 
     try {
-        const savedOrder = await newProduct.save()
+        const savedOrder = await newOrder.save()
         res.status(200).json(savedOrder)
     } catch(err) {
         res.status(500).json(err)
@@ -63,15 +63,30 @@ router.get("/", verifyTokenAndAdmin, async (req, res) => {
 
 //GET MONTHLY INCOME
 router.get("/income", verifyTokenAndAdmin, async (req, res) => {
-    const date = new Date()
-    const lastMonth = new Date(date.setMonth(date.getMonth()-1))
-    const previousMonth = new Date(new Date().setMonth(lastMonth.getMonth()-1))
-    
+    const date = new Date();
+    const lastMonth = new Date(date.setMonth(date.getMonth() - 1));
+    const previousMonth = new Date(new Date().setMonth(lastMonth.getMonth() - 1));
+
     try {
-
+        const income = await Order.aggregate([
+            { $match: { createdAt: { $gte: previousMonth } } },
+            {
+                $project: {
+                    month: { $month: "$createdAt" },
+                    sales: "$amount",
+                }
+            },
+            {
+                $group: {
+                    _id: "$month",
+                    total: { $sum: "$sales" }
+                }
+            }
+        ]);
+        res.status(200).json(income);
     } catch (err) {
-
+        res.status(500).json(err);
     }
-})
+});
 
 module.exports = router
